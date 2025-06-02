f6fd75b5cb5ab6611a971280e43615f844a6d989
Hrishi Panchal <hrishicoll3319@gmail.com>
2025-06-02 17:55:38 +0530

    Frontend Changes

--- docs/index.js
+++ docs/index.js
@@ -14,6 +14,7 @@ const ctx = canvas.getContext("2d")
 ctx.fillStyle = "black"
 ctx.fillRect(0, 0, WIDTH * SCALE, HEIGHT * SCALE)
 
+
 const input = document.getElementById("fileinput")
 
 async function run() {
